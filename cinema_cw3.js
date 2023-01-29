56dd0efe099d9cf933df1de2edf3a993b1a8aefa
yuywang9 <92161695+yuywang9@users.noreply.github.com>
2023-01-29 16:52:21 +0800

    Add files via upload

---
+++ cinema_cw3.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script>
+        s1 = 'I am a cityu student ';
+        s2 = '25';
+        s3 = 204444;
+    </script>
+</head>
+
+<body>
+    <script>
+        document.write(s1 + s2);
+        document.write(s1 - s3);
+    </script>
+</body>
+
+</html>
---
+++ movie.js
@@ -0,0 +1,65 @@
+function getMovies() {
+    return [
+        {
+            id: 1,
+            type: "now",
+            thumbnail: "image/casablanca.png",
+            src: "http://courses.cs.cityu.edu.hk/cs2204/video/casablanca-s.mp4",
+            name: "Casablanca",
+            cast: "Julius J.Epstein, Philip G. Epstein, Howard Koch",
+            director: "Michael Curtiz",
+            duration: "102 Min"
+        },
+        {
+            id: 2,
+            type: "now",
+            thumbnail: "image/bigbuck.jpg",
+            src: "http://courses.cs.cityu.edu.hk/cs2204/video/bigbuck-s.mp4",
+            name: "Big Buck",
+            cast: "Big Bunny, Little Fox",
+            director: "Blender People",
+            duration: "9 Min 56 sec"
+        },
+        {
+            id: 3,
+            type: "now",
+            thumbnail: "image/barbecue.jpg",
+            src: "http://courses.cs.cityu.edu.hk/cs2204/video/barbecue.mp4",
+            name: "Barbecue",
+            cast: "Delicious Meat",
+            director: "Your neighbour",
+            duration: "13 sec"
+        },
+
+        {
+            id: 4,
+            type: "upcoming",
+            thumbnail: "image/chocolate.jpg",
+            src: "http://courses.cs.cityu.edu.hk/cs2204/video/chocolate.mp4",
+            name: "Chocolate",
+            cast: "Hot Liquid chocolate",
+            director: "Pastry",
+            duration: "20 sec"
+        },
+        {
+            id: 5,
+            type: "upcoming",
+            thumbnail: "image/nature.jpg",
+            src: "http://courses.cs.cityu.edu.hk/cs2204/video/nature.mp4",
+            name: "Nature",
+            cast: " Ostrich, Kangaroo, Crocodile",
+            director: "Wildlife Researchers",
+            duration: "29 sec"
+        },
+        {
+            id: 6,
+            type: "upcoming",
+            thumbnail: "image/wonders.jpg",
+            src: "http://courses.cs.cityu.edu.hk/cs2204/video/wonders.mp4",
+            name: "Wonders",
+            cast: "Awesome Buildings, Joyful Teenagers",
+            director: "An Architect",
+            duration: "29 sec"
+        }
+    ];
+}
